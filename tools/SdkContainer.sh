56da0c46112e61f04e006191e1cb3c98302ac3d9
Ethan D. Twardy <ethan.twardy@gmail.com>
2021-09-26 16:53:08 -0500

    Updates to SdkContainer

--- tools/SdkContainer.sh
+++ tools/SdkContainer.sh
@@ -8,13 +8,15 @@
 #
 # CREATED:          09/24/2021
 #
-# LAST EDITED:      09/25/2021
+# LAST EDITED:      09/26/2021
 ###
 
+: "${SDK_VOLUME:=cortexa72-poky-linux}"
+
 podman run -it --rm $CONTAINER_USER \
-       -v cortexa72-poky-linux:/home/yocto/poky_sdk \
+       -v $SDK_VOLUME:/home/yocto/poky_sdk \
        -v $PWD:/home/yocto/app \
        -v $HOME/.gitconfig:/home/yocto/.gitconfig \
-       localhost/esdk-container
+       localhost/esdk-container $@
 
 ###############################################################################
