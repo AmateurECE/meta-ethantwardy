fba28de1be545f3885363bfa2d62711e0e675a5e
Ethan D. Twardy <ethan.twardy@gmail.com>
2025-01-16 20:53:56 -0600

    email-qemu: Create a script to ease running qemu with the btrfs drive

---
+++ tools/email-runqemu.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+usage() {
+	printf >&2 "Usage $0 -f <email-data-drive-image>\n"
+	exit 1
+}
+
+while getopts ":f:" opt; do
+	case "${opt}" in
+	f)
+		DATA_DRIVE_PATH=$(realpath ${OPTARG})
+		;;
+	*)
+		usage
+		;;
+	esac
+done
+
+if [ -z "${DATA_DRIVE_PATH}" ]; then
+	usage
+fi
+
+if [ ! -f kas/email-qemu.yaml ]; then
+	printf >&2 '%s\n' "Error: Run this in a directory where " \
+		"kas/email-qemu.yaml can be found"
+	exit 1
+fi
+
+DATA_DRIVE_ARGUMENTS="\
+	-drive if=none,id=hdata,file=${DATA_DRIVE_PATH},format=raw \
+	-device scsi-hd,drive=hdata"
+RUNQEMU_ARGUMENTS="\
+	tmp/deploy/images/qemux86-64/qemu-email-gadget-qemux86-64.rootfs.wic \
+	wic \
+	serial \
+	nographic \
+	slirp \
+	qemuparams=\"${DATA_DRIVE_ARGUMENTS}\""
+
+kas shell -c "runqemu ${RUNQEMU_ARGUMENTS}" kas/email-qemu.yaml
