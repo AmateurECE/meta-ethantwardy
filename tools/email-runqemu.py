6104c11722c81d63fd2a63477c7deb95be590382
Ethan D. Twardy <ethan.twardy@gmail.com>
2025-01-25 11:59:19 -0600

    email-gadget: Configure Guest networking between VM's

--- tools/email-runqemu.py
+++ tools/email-runqemu.py
@@ -1,6 +1,7 @@
 import argparse
 import configparser
 import os
+import socket
 
 SERIAL_OPTIONS=[
 	"-serial", "mon:stdio",
@@ -21,6 +22,14 @@ MACHINE_OPTIONS=[
     "-device", "virtio-scsi-pci,id=scsi",
 ]
 
+def someone_is_listening_on(address):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        try:
+            sock.bind(address)
+            return False
+        except Exception as e:
+            return True
+
 def data_drive_options(data_file: str):
     return [
         "-drive", f"if=none,id=hdata,file={data_file},format=raw",
@@ -35,10 +44,20 @@ def rootfs_options():
         "-device", "scsi-hd,drive=hd"
     ]
 
-def networking_options(mac_address: str):
+def networking_options(wan_mac: str, lan_mac: str):
+    address = ('0.0.0.0', 1234)
+    _, port = address
+    if someone_is_listening_on(address):
+        lan_spec = f"connect=127.0.0.1:{port}"
+    else:
+        lan_spec = f"listen=:{port}"
     return [
-        "-device", f"virtio-net-pci,netdev=net0,mac={mac_address}",
+        # User interface for accessing the internet
+        "-device", f"virtio-net-pci,netdev=net0,mac={wan_mac}",
         "-netdev", "user,id=net0",
+        # Guest network
+        "-device", f"virtio-net-pci,netdev=net1,mac={lan_mac}",
+        "-netdev", f"socket,id=net1,{lan_spec}",
     ]
 
 def main():
@@ -51,12 +70,13 @@ def main():
     config.read(args.file)
 
     data_file = config[args.config]['data_file']
-    mac_address = config[args.config]['mac_address']
+    wan_mac = config[args.config]['wan_mac']
+    lan_mac = config[args.config]['lan_mac']
 
     executable = '/usr/bin/qemu-system-x86_64'
     options = MACHINE_OPTIONS  + URANDOM_OPTIONS + rootfs_options() \
             + data_drive_options(data_file) \
-            + networking_options(mac_address) + SERIAL_OPTIONS
+            + networking_options(wan_mac, lan_mac) + SERIAL_OPTIONS
     os.execvp(executable, [executable] + options)
 
 if __name__ == '__main__':
