AmateurECE/meta-ethantwardy

commit 6ce58940702a7cfeeda4a9cfbc1470af9899be86

email-gadget: Improve postfix configuration customization
Previously, the postfix main.cf configuration file was an m4 template.
This was not flexible, in that it didn't allow deployments to change
arbitrary configuration. Now, there is a main.cf.base (installed by us)
and a main.cf.local which is simply appended to the base to form
main.cf. This file can contain arbitrary (valid) postfix configuration
that overrides the configuration parameters in the base file.

meta-email-gadget/recipes-daemons/postfix/postfix_%.bbappend
@@ -1,15 +1,24 @@
 FILESEXTRAPATHS:prepend := "${THISDIR}/files:"
 SRC_URI += " \
-    file://main.cf.template.m4 \
+    file://main.cf \
     file://master.cf \
+    file://postfix-cfg.sh \
 "
 
+INITSCRIPT_PACKAGES += "${PN}-cfg"
+INITSCRIPT_NAME:${PN}-cfg = "postfix-cfg"
+
 do_install:append() {
-    install -Dm644 ${UNPACKDIR}/main.cf.template.m4 -t ${D}/etc/gadget
+    install -Dm644 ${UNPACKDIR}/main.cf ${D}/etc/postfix/main.cf.base
     install -Dm644 ${UNPACKDIR}/master.cf -t ${D}/etc/postfix
 
     rm -f ${D}/etc/postfix/main.cf
+
+    install -Dm755 ${UNPACKDIR}/postfix-cfg.sh ${D}/etc/init.d/postfix-cfg
 }
 
 FILES:${PN}-cfg:remove = "/etc/postfix/main.cf"
-FILES:${PN}-cfg:add = "/etc/gadget/main.cf.template.m4"
+FILES:${PN}-cfg:append = " \
+    /etc/postfix/main.cf.base \
+    /etc/init.d/postfix-cfg \
+"

meta-email-gadget/recipes-core/images/email-gadget-image.bb
@@ -51,9 +51,6 @@ IMAGE_INSTALL += " \
 # Bind9 as a recursive DNS nameserver
 IMAGE_INSTALL += " bind"
 
-# Build configuration at runtime
-IMAGE_INSTALL += " make-conf"
-
 # Nginx as a webserver
 IMAGE_INSTALL += "nginx"
 

meta-email-gadget/recipes-core/make-conf/make-conf.bb
@@ -1,17 +1,0 @@
-SUMMARY = "Make configuration files at runtime"
-LICENSE = "MIT"
-LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/MIT;md5=0835ade698e0bcf8506ecda2f7b4f302"
-SRC_URI = "file://make-conf.sh"
-
-S = "${WORKDIR}/sources"
-UNPACKDIR = "${S}"
-
-RDEPENDS:${PN} = "m4"
-
-do_install() {
-    install -Dm755 ${UNPACKDIR}/make-conf.sh ${D}${sysconfdir}/init.d/make-conf
-}
-
-inherit update-rc.d
-
-INITSCRIPT_NAME = "make-conf"

meta-email-gadget/recipes-daemons/postfix/files/main.cf
@@ -2,8 +2,6 @@ compatibility_level = 3.10
 smtputf8_enable = no
 
 # Domain
-myhostname = MYHOSTNAME
-mydomain = MYDOMAIN
 mydestination = $myhostname localhost.$mydomain localhost
 mynetworks = 127.0.0.0/8
 mynetworks_style = host

meta-email-gadget/recipes-daemons/postfix/files/postfix-cfg.sh
@@ -4,13 +4,13 @@
 #
 
 ### BEGIN INIT INFO
-# Provides:             make-conf
+# Provides:             postfix-cfg
 # Required-Start:       $local_fs
 # Required-Stop:        $local_fs
 # Default-Start:        S
 # Default-Stop:
-# Short-Description:  Generate configuration data for runtime use
+# Short-Description:  Generate the postfix configuration
 ### END INIT INFO
 
 # Generate the postfix configuration file
-m4 /etc/gadget/main.cf.vars.m4 /etc/gadget/main.cf.template.m4 >/etc/postfix/main.cf
+cat /etc/postfix/main.cf.base /etc/postfix/main.cf.local >/etc/postfix/main.cf

commit 628b5848e948d65b0190d5ac3efeaaf656e6dc1a

email-gadget: Fix postfix init script
The linked commit disabled the postfix init script, which is
unfortunately already installed in the postfix-cfg package. Add a new
package for this initscript.

Fixes: 6ce58940702a7cfeeda4a9cfbc1470af9899be86

meta-email-gadget/recipes-daemons/postfix/postfix_%.bbappend
@@ -2,11 +2,16 @@ FILESEXTRAPATHS:prepend := "${THISDIR}/files:"
 SRC_URI += " \
     file://main.cf \
     file://master.cf \
-    file://postfix-cfg.sh \
+    file://postfix-makeconf.sh \
 "
 
-INITSCRIPT_PACKAGES += "${PN}-cfg"
-INITSCRIPT_NAME:${PN}-cfg = "postfix-cfg"
+PACKAGES =+ "${PN}-makeconf"
+
+# Upstream recipe uses INITSCRIPT_NAME without a package name.
+# /etc/init.d/postfix is installed in postfix-cfg package.
+INITSCRIPT_PACKAGES += "${PN}-cfg ${PN}-makeconf"
+INITSCRIPT_NAME:${PN}-cfg = "postfix"
+INITSCRIPT_NAME:${PN}-makeconf = "postfix-makeconf"
 
 do_install:append() {
     install -Dm644 ${UNPACKDIR}/main.cf ${D}/etc/postfix/main.cf.base
@@ -14,11 +19,15 @@ do_install:append() {
 
     rm -f ${D}/etc/postfix/main.cf
 
-    install -Dm755 ${UNPACKDIR}/postfix-cfg.sh ${D}/etc/init.d/postfix-cfg
+    install -Dm755 ${UNPACKDIR}/postfix-makeconf.sh ${D}/etc/init.d/postfix-makeconf
 }
 
 FILES:${PN}-cfg:remove = "/etc/postfix/main.cf"
 FILES:${PN}-cfg:append = " \
     /etc/postfix/main.cf.base \
-    /etc/init.d/postfix-cfg \
 "
+
+FILES:${PN}-makeconf = " \
+    /etc/init.d/postfix-makeconf \
+"
+RDEPENDS:${PN}-cfg += "${PN}-makeconf"