26e196104a8ebe371d99a75607fe0d29a12fc5a0
Ethan D. Twardy <ethan.twardy@gmail.com>
2025-06-26 21:17:11 -0500

    gadget: x86-64-mutable-gadget: Fix RAUC boot
    * Use an ext4 image in the bundle, so that we can label the partition
      appropriately during bundle generation. This is required so that the
      partition can be re-labeled by the rauc-label-handler
      post-installation.
    * Set the correct path to the grubenv file
    * Sleep for 1 second in the rauc-label-handler to allow the kernel a
      little extra time to reload the partition table.

--- meta-gadget/recipes-bsp/rauc-label-handler/files/rauc-label-handler.sh
+++ meta-gadget/recipes-bsp/rauc-label-handler/files/rauc-label-handler.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+# Sleep to give the kernel time to reload the partition table on slow disks
+sleep 1
 eval TARGET_LABEL=\$RAUC_SLOT_BOOTNAME_${RAUC_TARGET_SLOTS}
 e2label /dev/disk/by-label/rootfs-bundle ${TARGET_LABEL}
