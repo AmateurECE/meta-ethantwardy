d70e77ba650cdbb356e8947831cb52194a7886b0
Ethan D. Twardy <ethan.twardy@gmail.com>
2026-03-20 19:42:19 -0500

    feat(backup-gadget): btrfs scrub/balance
    Add monthly scheduling to jobs in backup-controller. Fix the tests to
    use fixed timezone offsets, and make Job scheduling timezone generic.

--- layers/meta-backup-gadget/src/backup-controller/src/job.rs
+++ layers/meta-backup-gadget/src/backup-controller/src/job.rs
@@ -1,10 +1,11 @@
 use std::process::Command;
 
-use chrono::{DateTime, Datelike as _, Local, NaiveTime, TimeDelta, Weekday};
+use chrono::{DateTime, Datelike as _, Local, NaiveTime, TimeDelta, TimeZone, Weekday};
 
 pub enum Schedule {
     Daily(NaiveTime),
     Weekly { weekday: Weekday, time: NaiveTime },
+    Monthly { day_of_month: u32, time: NaiveTime },
 }
 
 pub struct Job {
@@ -13,7 +14,7 @@ pub struct Job {
 }
 
 impl Job {
-    pub fn next_invocation(&self, after: &DateTime<Local>) -> DateTime<Local> {
+    pub fn next_invocation<Tz: TimeZone>(&self, after: &DateTime<Tz>) -> DateTime<Tz> {
         let time_until_invocation_tomorrow = |time: NaiveTime| {
             if time > after.time() {
                 time - after.time()
@@ -32,7 +33,7 @@ impl Job {
                     if time > after.time() {
                         hours
                     } else {
-                        hours + TimeDelta::days((6 - (today - weekday)).into())
+                        hours + TimeDelta::days((7 - (today - weekday) - 1).into())
                     }
                 } else if today < weekday {
                     hours + TimeDelta::days((weekday - today).into())
@@ -40,9 +41,38 @@ impl Job {
                     hours + TimeDelta::days((7 - (today - weekday)).into())
                 }
             }
+
+            Schedule::Monthly { day_of_month, time } => {
+                let hours = time_until_invocation_tomorrow(time);
+                if after.day() == day_of_month {
+                    // It's today! When??
+                    if time > after.time() {
+                        // It's so close!
+                        hours
+                    } else {
+                        // Oh, no actually it's next month
+                        hours
+                            + TimeDelta::days(
+                                (u32::from(after.num_days_in_month()) - after.day() + day_of_month
+                                    - 1)
+                                .into(),
+                            )
+                    }
+                } else if after.day() < day_of_month {
+                    // Not yet, but it's coming up!
+                    hours + TimeDelta::days((day_of_month - after.day()).into())
+                } else {
+                    // It's next month!
+                    hours
+                        + TimeDelta::days(
+                            (u32::from(after.num_days_in_month()) - after.day() + day_of_month - 1)
+                                .into(),
+                        )
+                }
+            }
         };
 
-        *after + time_until_next
+        after.clone() + time_until_next
     }
 
     pub fn is_due_within(&self, delta: &TimeDelta) -> bool {
@@ -65,55 +95,78 @@ pub fn next_job<'a>(after: &DateTime<Local>, jobs: &'a [Job]) -> Option<&'a Job>
 
 #[cfg(test)]
 mod test {
+    use chrono::{FixedOffset, TimeZone};
+
     use super::*;
+
+    const OFFSET: i32 = 5 * 3600;
 
     #[test]
     fn daily_job_lt() {
         assert_eq!(
-            "2026-03-15T04:45:00-05:00"
-                .parse::<DateTime<Local>>()
+            FixedOffset::west_opt(OFFSET)
+                .unwrap()
+                .with_ymd_and_hms(2026, 03, 15, 4, 45, 0)
                 .unwrap(),
             Job {
                 command: Box::new(|| Command::new("ls")),
                 schedule: Schedule::Daily("04:45:00".parse().unwrap()),
             }
-            .next_invocation(&"2026-03-15T02:07:47-05:00".parse().unwrap(),)
+            .next_invocation(
+                &FixedOffset::west_opt(OFFSET)
+                    .unwrap()
+                    .with_ymd_and_hms(2026, 03, 15, 02, 07, 47)
+                    .unwrap()
+            )
         )
     }
 
     #[test]
     fn daily_job_gt() {
         assert_eq!(
-            "2026-03-16T04:45:00-05:00"
-                .parse::<DateTime<Local>>()
+            FixedOffset::west_opt(OFFSET)
+                .unwrap()
+                .with_ymd_and_hms(2026, 03, 16, 4, 45, 0)
                 .unwrap(),
             Job {
                 command: Box::new(|| Command::new("ls")),
                 schedule: Schedule::Daily("04:45:00".parse().unwrap()),
             }
-            .next_invocation(&"2026-03-15T04:45:07-05:00".parse().unwrap(),)
+            .next_invocation(
+                &FixedOffset::west_opt(OFFSET)
+                    .unwrap()
+                    .with_ymd_and_hms(2026, 03, 15, 04, 45, 07)
+                    .unwrap()
+            )
         )
     }
 
     #[test]
     fn daily_job_eq() {
         assert_eq!(
-            "2026-03-16T04:45:00-05:00"
-                .parse::<DateTime<Local>>()
+            FixedOffset::west_opt(OFFSET)
+                .unwrap()
+                .with_ymd_and_hms(2026, 03, 16, 4, 45, 0)
                 .unwrap(),
             Job {
                 command: Box::new(|| Command::new("ls")),
                 schedule: Schedule::Daily("04:45:00".parse().unwrap()),
             }
-            .next_invocation(&"2026-03-15T04:45:00-05:00".parse().unwrap(),)
+            .next_invocation(
+                &FixedOffset::west_opt(OFFSET)
+                    .unwrap()
+                    .with_ymd_and_hms(2026, 03, 15, 04, 45, 0)
+                    .unwrap()
+            )
         )
     }
 
     #[test]
     fn weekly_job_lt_sameday() {
         assert_eq!(
-            "2026-03-22T07:45:00-05:00"
-                .parse::<DateTime<Local>>()
+            FixedOffset::west_opt(OFFSET)
+                .unwrap()
+                .with_ymd_and_hms(2026, 03, 22, 07, 45, 0)
                 .unwrap(),
             Job {
                 command: Box::new(|| Command::new("/usr/libexec/backup-databases.sh")),
@@ -122,15 +175,21 @@ mod test {
                     time: "07:45:00".parse().unwrap(),
                 }
             }
-            .next_invocation(&"2026-03-16T07:43:47-05:00".parse().unwrap(),)
+            .next_invocation(
+                &FixedOffset::west_opt(OFFSET)
+                    .unwrap()
+                    .with_ymd_and_hms(2026, 03, 16, 7, 43, 47)
+                    .unwrap()
+            )
         )
     }
 
     #[test]
     fn weekly_job_lt() {
         assert_eq!(
-            "2026-03-16T07:45:00-05:00"
-                .parse::<DateTime<Local>>()
+            FixedOffset::west_opt(OFFSET)
+                .unwrap()
+                .with_ymd_and_hms(2026, 03, 16, 07, 45, 0)
                 .unwrap(),
             Job {
                 command: Box::new(|| Command::new("ls")),
@@ -139,15 +198,21 @@ mod test {
                     time: "07:45:00".parse().unwrap()
                 },
             }
-            .next_invocation(&"2026-03-15T04:02:23-05:00".parse().unwrap(),)
+            .next_invocation(
+                &FixedOffset::west_opt(OFFSET)
+                    .unwrap()
+                    .with_ymd_and_hms(2026, 03, 15, 04, 02, 23)
+                    .unwrap()
+            )
         )
     }
 
     #[test]
     fn weekly_job_gt() {
         assert_eq!(
-            "2026-03-23T07:45:00-05:00"
-                .parse::<DateTime<Local>>()
+            FixedOffset::west_opt(OFFSET)
+                .unwrap()
+                .with_ymd_and_hms(2026, 03, 23, 07, 45, 0)
                 .unwrap(),
             Job {
                 command: Box::new(|| Command::new("ls")),
@@ -156,15 +221,21 @@ mod test {
                     time: "07:45:00".parse().unwrap()
                 },
             }
-            .next_invocation(&"2026-03-16T07:45:23-05:00".parse().unwrap(),)
+            .next_invocation(
+                &FixedOffset::west_opt(OFFSET)
+                    .unwrap()
+                    .with_ymd_and_hms(2026, 03, 16, 7, 45, 23)
+                    .unwrap()
+            )
         )
     }
 
     #[test]
     fn weekly_job_eq() {
         assert_eq!(
-            "2026-03-23T07:45:00-05:00"
-                .parse::<DateTime<Local>>()
+            FixedOffset::west_opt(OFFSET)
+                .unwrap()
+                .with_ymd_and_hms(2026, 03, 23, 7, 45, 0)
                 .unwrap(),
             Job {
                 command: Box::new(|| Command::new("ls")),
@@ -173,7 +244,104 @@ mod test {
                     time: "07:45:00".parse().unwrap()
                 },
             }
-            .next_invocation(&"2026-03-16T07:45:00-05:00".parse().unwrap(),)
+            .next_invocation(
+                &FixedOffset::west_opt(OFFSET)
+                    .unwrap()
+                    .with_ymd_and_hms(2026, 03, 16, 07, 45, 00)
+                    .unwrap()
+            )
+        )
+    }
+
+    #[test]
+    fn monthly_job_lt_sameday() {
+        assert_eq!(
+            FixedOffset::west_opt(5 * 3600)
+                .unwrap()
+                .with_ymd_and_hms(2026, 03, 01, 07, 45, 00)
+                .unwrap(),
+            Job {
+                command: Box::new(|| Command::new("/usr/libexec/backup-databases.sh")),
+                schedule: Schedule::Monthly {
+                    day_of_month: 1,
+                    time: "07:45:00".parse().unwrap(),
+                }
+            }
+            .next_invocation(
+                &FixedOffset::west_opt(OFFSET)
+                    .unwrap()
+                    .with_ymd_and_hms(2026, 03, 01, 07, 43, 47)
+                    .unwrap()
+            )
+        )
+    }
+
+    #[test]
+    fn monthly_job_lt() {
+        assert_eq!(
+            FixedOffset::west_opt(OFFSET)
+                .unwrap()
+                .with_ymd_and_hms(2026, 04, 02, 07, 45, 00)
+                .unwrap(),
+            Job {
+                command: Box::new(|| Command::new("ls")),
+                schedule: Schedule::Monthly {
+                    day_of_month: 2,
+                    time: "07:45:00".parse().unwrap()
+                },
+            }
+            .next_invocation(
+                &FixedOffset::west_opt(OFFSET)
+                    .unwrap()
+                    .with_ymd_and_hms(2026, 04, 01, 04, 02, 23)
+                    .unwrap()
+            )
+        )
+    }
+
+    #[test]
+    fn monthly_job_gt() {
+        assert_eq!(
+            FixedOffset::west_opt(OFFSET)
+                .unwrap()
+                .with_ymd_and_hms(2026, 04, 01, 07, 45, 00)
+                .unwrap(),
+            Job {
+                command: Box::new(|| Command::new("ls")),
+                schedule: Schedule::Monthly {
+                    day_of_month: 1,
+                    time: "07:45:00".parse().unwrap()
+                },
+            }
+            .next_invocation(
+                &FixedOffset::west_opt(OFFSET)
+                    .unwrap()
+                    .with_ymd_and_hms(2026, 03, 16, 07, 45, 23)
+                    .unwrap()
+            )
+        )
+    }
+
+    #[test]
+    fn monthly_job_eq() {
+        assert_eq!(
+            FixedOffset::west_opt(OFFSET)
+                .unwrap()
+                .with_ymd_and_hms(2026, 04, 01, 07, 45, 00)
+                .unwrap(),
+            Job {
+                command: Box::new(|| Command::new("ls")),
+                schedule: Schedule::Monthly {
+                    day_of_month: 1,
+                    time: "07:45:00".parse().unwrap()
+                },
+            }
+            .next_invocation(
+                &FixedOffset::west_opt(OFFSET)
+                    .unwrap()
+                    .with_ymd_and_hms(2026, 03, 01, 07, 45, 00)
+                    .unwrap()
+            )
         )
     }
 }
